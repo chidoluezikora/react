8ef2eab0c8c3a3e18024039ba31fa14b7f80109c
chidoluezikora <chidozik2002@gmail.com>
2023-02-23 15:09:32 +0100

    allow sorting of pets

--- adopt-a-pet/src/App.js
+++ adopt-a-pet/src/App.js
@@ -8,6 +8,7 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Root/>} >
     <Route path='/' element={<HomePage/>} />
+    <Route path=':type' element={<HomePage/>} />
   </Route>
 ));
 
