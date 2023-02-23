c8979decc86c506d2e2da6d6c7220895d0c18bd0
chidoluezikora <chidozik2002@gmail.com>
2023-02-23 14:01:33 +0100

    use useNavigate to navigate user history and navigate imperatively

--- react-router-lesson/src/components/Footer.js
+++ react-router-lesson/src/components/Footer.js
@@ -1,18 +1,10 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 export default function Footer() {
-
-  // get the navigate function
-
-  const goBack = () => {
-    // imperatively redirect back
-    
-  }
-
-  const goForward = () => {
-    // imperatively redirect forward
-    
-  }
+  const navigate = useNavigate();
+  const goBack = () => navigate(-1);
+  const goForward = () => navigate(1);
 
   return (
     <footer>
